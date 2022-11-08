6d5d8421903a7a96e67509914a7b07915d0e09a0
Mauro Baptista <mauro@carnou.com>
2022-11-08 09:27:06 -0300

    Added option to custom domain

--- resources/views/components/layouts/blog.blade.php
+++ resources/views/components/layouts/blog.blade.php
@@ -22,8 +22,8 @@
 
     @vite('resources/css/app.css')
 
-    @if (config('services.fathom.site'))
-        <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="{{ config('services.fathom.site') }}" defer=""></script>
+    @if (config('services.fathom.domain') && config('services.fathom.site'))
+        <script src="https://{{ config('services.fathom.domain') }}/script.js" data-spa="auto" data-site="{{ config('services.fathom.site') }}" defer=""></script>
     @endif
 
     @isset($meta)

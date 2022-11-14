17bb923bfb7d1e2541a97fc59b3814345f54f00b
Mauro Baptista <mauro@carnou.com>
2022-11-14 09:24:21 -0300

    Added tests

---
+++ tests/Feature/Http/Controllers/Sitemap/IndexControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Post;
+
+use App\Models\Post;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SitemapControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function xml_builder()
+    {
+        Post::factory()->published()->create([
+            'title' => 'This is a test',
+            'slug' => 'this-is-a-test',
+            'updated_at' => '2022-10-10 10:10:10',
+        ]);
+
+        Post::factory()->published()->create([
+            'title' => 'This is another test',
+            'slug' => 'this-is-another-test',
+            'updated_at' => '2022-10-11 10:10:10',
+        ]);
+
+        Post::factory()->draft()->create([
+            'title' => 'This is a non-released test',
+            'slug' => 'this-is-a-non-released-test',
+            'updated_at' => '2022-10-12 10:10:10',
+        ]);
+
+        $response = $this->get('/sitemap.xml');
+
+        $response->assertStatus(200);
+
+        $sitemapXml = simplexml_load_string($response->content());
+        $sitemap = json_decode(json_encode($sitemapXml), true)['url'];
+
+        $expected = [[
+            'loc' => 'https://maurobaptista.com/',
+            'lastmod' => '2022-10-11T10:10:10+00:00',
+            'changefreq' => 'weekly',
+            'priority' => '1.00',
+        ], [
+            'loc' => 'https://maurobaptista.com/posts',
+            'lastmod' => '2022-10-11T10:10:10+00:00',
+            'changefreq' => 'weekly',
+            'priority' => '0.80',
+        ], [
+            'loc' => 'https://maurobaptista.localhost/posts/this-is-another-test',
+            'lastmod' => '2022-10-11T10:10:10+00:00',
+            'priority' => '0.80',
+        ], [
+            'loc' => 'https://maurobaptista.localhost/posts/this-is-a-test',
+            'lastmod' => '2022-10-10T10:10:10+00:00',
+            'priority' => '0.80',
+        ]];
+
+        $this->assertEquals($expected, $sitemap);
+    }
+}
